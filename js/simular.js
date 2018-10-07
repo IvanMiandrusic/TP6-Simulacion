2381060ad0ac7362d60f023647848de0f33772d1
Ivan <imiandrusic@gmail.com>
2018-10-07 00:20:08 -0300

    FIrst Commit

--- js/simular.js
+++ js/simular.js
@@ -17,12 +17,12 @@ var tiempoEstadia = 0;
 //Var Auxiliares
 var stll = 0;
 var sts = 0;
-var inicioTO = 0;
-var tiempoOscioso = 0;
+var tiempoOsciosoTotal = 0;
 var totalArribos = 0;
 
 var tiempo = 0; //Tiempos en segundos
-var tiempoFinal = 50;
+var tiempoFinal = 0;
+var arrepentidos = 0;
 
 function simular(){
 
@@ -39,14 +39,17 @@ function simular(){
             stll += tpll;
             nroTotalGenteEnElSistema+=1;
             totalArribos += 1;
-            intervaloArribos = generateIA();
-            tpll = tiempo + intervaloArribos;
-            var mesaWithMNP = getMesaWithASlot();
-            var mesa = arrMesas[mesaWithMNP['nroMesa']];
-            if(mesaWithMNP['nroPersonasEnLaMesa'] <= capMesas){
-                addTEaUnaMesa(mesa, mesaWithMNP['nroMesa']);
+            if(nroTotalGenteEnElSistema>=nroMesas*capMesas) {
+                var randomDecicion = Math.random();
+                if(randomDecicion > 0.1){
+                    arrepentidos += 1;
+                    intervaloArribos = generateIA();
+                    tpll = tiempo + intervaloArribos;
+                }else{
+                    seQueda();
+                }
             }else{
-                //TODO
+                seQueda();
             }
         }else{
             tiempo = tps;
@@ -54,7 +57,7 @@ function simular(){
             if(nroTotalGenteEnElSistema>0) {
                 nroTotalGenteEnElSistema -= 1;
             }
-            mesa = arrMesas[minTPS['nroMesa']];
+            var mesa = arrMesas[minTPS['nroMesa']];
             if(mesa['nroPersonasEnLaMesa']>0 && mesa['personas'].length>0) {
                 mesa['nroPersonasEnLaMesa'] -= 1;
                 mesa['personas'].shift();
@@ -65,19 +68,31 @@ function simular(){
                 //TODO
             }
         }
-
-        if(tiempo>tiempoFinal){
+        if(tiempo<tiempoFinal && tpll < tiempoFinal){
+            procesarInformacion();
+            return false;
+        }else{
             if(nroTotalGenteEnElSistema==0){
                 calcularResultados();
+                return false;
             }else{
                 tpll = 100000000000000000000000;
                 procesarInformacion();
             }
-
-        }else{
-            procesarInformacion();
         }
+    }
 
+
+    function seQueda(){
+        intervaloArribos = generateIA();
+        tpll = tiempo + intervaloArribos;
+        var mesaWithMNP = getMesaWithASlot();
+        var mesa = arrMesas[mesaWithMNP['nroMesa']];
+        if(mesaWithMNP['nroPersonasEnLaMesa'] <= capMesas){
+            addTEaUnaMesa(mesa, mesaWithMNP['nroMesa']);
+        }else{
+            //TODO
+        }
     }
 
     function addTEaUnaMesa(mesa, nroMesa){
@@ -85,18 +100,26 @@ function simular(){
             var tiempoEstadia = generateTE();
             mesa['personas'].push(tiempo + tiempoEstadia);
             mesa['personas'].sort(function(a, b){return a - b});
-            console.log('personas: '+mesa['personas']);
             mesa['nroPersonasEnLaMesa']+=1;
+            mesa['totalPersonas']+=1;
             arrMesas[nroMesa] = mesa;
         }
     }
 
     function generateIA(){
-        return (Math.floor(Math.random() * 100) + 1);
+        var alfa = 0.47576;
+        var beta = 1.752;
+        var random = Math.random();
+        return parseInt(((beta/(Math.pow(1-random, (1/alfa)))))-beta);
+
     }
 
     function generateTE(){
-        return (Math.floor(Math.random() * 100) + 1);
+        var alfa = 2.8588;
+        var beta = 11683;
+        var k =87.364;
+        var random = Math.random();
+        return parseInt((Math.pow((Math.pow(1-random, (-(1/k))))-1, 1/alfa))*beta);
     }
 
     function getMesaWithASlot(){
@@ -107,13 +130,6 @@ function simular(){
         }else{
             responseMesa = getMesaWithASlot();
         }
-
-        //for(var i=0; i<arrMesas.length;i++){
-        //    if(minNroPersonas > arrMesas[i]['nroPersonasEnLaMesa'] && arrMesas[i]['nroPersonasEnLaMesa'] < capMesas){
-        //        minNroPersonas = arrMesas[i]['nroPersonasEnLaMesa'];
-        //        responseMesa = {'nroPersonasEnLaMesa':arrMesas[i]['nroPersonasEnLaMesa'], 'nroMesa':i};
-        //    }
-        //}
         return responseMesa;
     }
 
@@ -137,23 +153,43 @@ function simular(){
         $('#spanCapMesas').text(capMesas);
         $('#spanTiempoFinalizacion').text(tiempo);
         $('#spanTiempoEsperadoFinalizacion').text(tiempoFinal);
+        $('#spanCantArrepentidos').text(arrepentidos);
+        $('#spanTotArribos').text(totalArribos);
+        $('#spanPPS').text(parseInt((sts-stll)/totalArribos));
+        for(var i=0; i<arrMesas.length;i++){
+            var promedioUso = parseInt((arrMesas[i]['totalPersonas']/totalArribos)*100);
 
+            var itemMesa = '<div class="progress"><label style="margin-right: 10px">Mesa '+(i+1)+':</label>'+
+                            '<div class="progress-bar progress-bar-striped bg-info" role="progressbar" aria-valuenow="'+promedioUso+'" aria-valuemin="0" aria-valuemax="100" style="width:'+promedioUso+'%">'+
+                            promedioUso+'% '+
+                            '</div>'+
+                            '</div><br>';
+
+            //var itemMesa = '<li class="list-group-item">Mesa '+(i+1)+':   <strong>'+promedioUso+'%</strong></li>';
+            $('#toMesas').append(itemMesa);
+        }
         $('#content2').show();
     }
 
     function inicializarVariables(){
         tpll = 0;
         tps = 0;
+        stll = 0;
+        sts = 0;
         nroTotalGenteEnElSistema = 0;
         intervaloArribos = 0;
         tiempoEstadia = 0;
         tiempo = 0; //Tiempos en segundos
-        tiempoFinal = 5000;
+        tiempoFinal = 7200;
+        arrepentidos = 0;
+        tiempoOsciosoTotal = 0;
         nroMesas= parseInt($('#nroMesas').val());
         capMesas = parseInt($('#capMesas').val());
         arrMesas = new Array(nroMesas);
+        totalArribos = 0;
+        $('#toMesas').empty();
         for(var i=0; i<arrMesas.length ;i++){
-            arrMesas[i] = {'personas':[],'nroPersonasEnLaMesa':0};
+            arrMesas[i] = {'personas':[],'nroPersonasEnLaMesa':0, 'totalPersonas': 0};
         }
     }
 
@@ -163,3 +199,7 @@ function simular(){
 
 }
 
+function nuevaSimulacion(){
+    $('#content').show();
+    $('#content2').hide();
+}
